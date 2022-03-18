b4b6198bd5b13d68a1ec6d3baa4b8359139ad095
Eric Liu <me@fxlui.com>
2022-03-19 02:41:36 +0800

    removed buggy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import compress from "vite-plugin-compress";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), compress()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
